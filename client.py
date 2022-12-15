ef46fbb827186222054135bc270960ca999fbd01
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-12-15 19:00:23 +0200

    error message

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import json
 public_key,private_key = rsa.newkeys(1024)
 print(private_key,public_key,sep="\n"*3)
 
-server = "http://127.0.0.1:5000"
+server = "https://web-production-39c4.up.railway.app"
 
 respone = (requests.get(server+"/",params={"public_key":base64.b64encode(pickle.dumps(public_key))}).content)
 data_block= json.loads(rsa.decrypt(respone,priv_key=private_key).decode(encoding="ascii"))
--- server.py
+++ server.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET"])
 def hello_world():
     public_key = pickle.loads(base64.b64decode(request.args.get("public_key")))
+    if not public_key : 
+        return "<p> you must enter public key <\p> "
     print(public_key,type(public_key))
     respone = rsa.encrypt(json.dumps({"size":"800x800", "sindex": "100x10" ,"0":+3 , "1":-3 }).encode(),pub_key=public_key)
     print(respone)
